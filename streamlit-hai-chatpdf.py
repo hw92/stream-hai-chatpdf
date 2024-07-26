54edd29f050c74f7dc816bedb684cb0a6bdb28b2
hw92 <dev@h5g.us>
2024-07-26 15:11:20 +0800

    update streamlit app

--- streamlit-hai-chatpdf.py
+++ streamlit-hai-chatpdf.py
@@ -95,7 +95,7 @@ def build_vector_store(pdf_text):
     #     path="./local_qdrant",
     #     collection_name="my_documents",
     # )
-
+    
 def build_qa_model(llm):
     qdrant = load_qdrant()
     retriever = qdrant.as_retriever(
